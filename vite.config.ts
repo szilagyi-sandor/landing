a4a9deb1f1b920f15673dea65f759c20822dc3cc
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-10-02 17:08:05 +0200

    add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      overlay: {
+        initialIsOpen: false,
+      },
+      eslint: {
+        lintCommand:
+          "eslint --cache --cache-location ./node_modules/.cache/eslint src",
+      },
+    }),
+  ],
 });
 
 // CHECKED 1.0
