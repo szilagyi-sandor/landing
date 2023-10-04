a27cd15d6201a0221a611fd110601da49ef78814
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-10-04 18:56:10 +0200

    fix: update logic for checked comment linting

--- plugins/ssm-eslint/requiredCheckedComment.js
+++ plugins/ssm-eslint/requiredCheckedComment.js
@@ -1,7 +1,19 @@
 // TODO: get version from file
 const VERSION_TO_LOOK_FOR = "1.0";
+const CHECKED_COMMENT_TEXT = ` CHECKED ${VERSION_TO_LOOK_FOR}`;
+const CHECKED_COMMENT = `//${CHECKED_COMMENT_TEXT}`;
 
-const getNodeEnd = (node) => node.range[1];
+const checkIfCheckedCommentIsLast = (sourceCode) => {
+  const escapedText = sourceCode.text.replace(/\r\n|\r|\n/g, "");
+
+  const indexOfCheckedComment = escapedText.indexOf(CHECKED_COMMENT);
+
+  if (indexOfCheckedComment < 0) {
+    return false;
+  }
+
+  return indexOfCheckedComment + CHECKED_COMMENT.length === escapedText.length;
+};
 
 // TODO: add suggestion
 // TODO: make this fixable
@@ -16,10 +28,8 @@ module.exports = {
       Program(node) {
         // context.sourceCode.getCommentsAfter() is not working properly
 
-        const nodeEnd = getNodeEnd(node);
-
         const checkedComment = node.comments.find(
-          (comment) => comment.value === ` CHECKED ${VERSION_TO_LOOK_FOR}`
+          (comment) => comment.value === CHECKED_COMMENT_TEXT
         );
 
         if (!checkedComment) {
@@ -27,7 +37,7 @@ module.exports = {
             node,
             message: "the checked comment is missing",
           });
-        } else if (getNodeEnd(checkedComment) < nodeEnd - 1) {
+        } else if (!checkIfCheckedCommentIsLast(context.sourceCode)) {
           context.report({
             node,
             message: "the checked comment should be at the end",
