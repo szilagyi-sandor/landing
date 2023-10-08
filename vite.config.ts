0c5b9974293b25ed0a0874e14fb6467cf187e5e9
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-10-08 18:46:23 +0200

    fix @aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     checker({
       typescript: true,
       overlay: {
